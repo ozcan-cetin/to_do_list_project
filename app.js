248540b60fa3a5aab186b64c94fdbea51676690d
özcan çetin <cetin445@gmail.com>
2022-06-07 02:37:13 +0300

    capturing method added

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ let add = document.querySelector(".add");
 let list = document.querySelector(".list");
 let warning = document.querySelector(".warning");
 let clearBtn = document.querySelector("#clearBtn");
+
+let totalCount=0;
+let checkedCount=0;
+
+//* FUNCTIONS *//
+list.addEventListener("click" ,(e)=>{
+    if(e.target.classList.contains("checkbox")){
+        if(e.target.checked){
+            e.target.nextElementSibling.style.textDecoration="line-through";
+            checkedCount++
+        }else{
+            e.target.nextElementSibling.style.textDecoration="none";
+            checkedCount--
+        }
+    }
+    else if(e.target.classList.contains("remove")){
+        (e.target.closest(".line").style.display="none")
+    }
+})
