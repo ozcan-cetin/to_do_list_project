70d29a2e7e70586dee8c69fab41e7695723e4bbc
özcan çetin <cetin445@gmail.com>
2022-06-07 02:34:42 +0300

    variables defined

---
+++ app.js
@@ -0,0 +1,6 @@
+//* VARIABLES *//
+let text =document.querySelector("#text");
+let add = document.querySelector(".add");
+let list = document.querySelector(".list");
+let warning = document.querySelector(".warning");
+let clearBtn = document.querySelector("#clearBtn");
